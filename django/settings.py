721b2faf4ab6ed356be89cb3695e6e7aa37523d7
Omen Wild <omen@ucdavis.edu>
2016-04-26 11:44:05 -0700

    Moved the serving location into /srv/ .

--- django/settings.py
+++ django/settings.py
@@ -135,5 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    '/var/www/django_www/static/',
+    '/srv/django_dev/static/',
 ]
