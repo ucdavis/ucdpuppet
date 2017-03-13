3870ad325ee6cef419c8be78f1e4f9f363bcc6d0
root <root@ucdpuppet.cse.ucdavis.edu>
2017-03-13 13:29:53 -0700

    added migation for CVE-2017-2636 and floppy exploit

--- puppet/manifests/useucdpuppet.pp
+++ puppet/manifests/useucdpuppet.pp
@@ -7,26 +7,32 @@
  
 class ucdpuppet::useucdpuppet
 {
-	augeas { "puppet.conf":
-      context => "/files/etc/puppetlabs/puppet/puppet.conf/agent",
-      changes => ["set server puppet.ucdavis.edu"]
-   }
-	service { 'puppet':
-		enable => true,
-	}
-	case $operatingsystem {
-			  /^(Debian|Ubuntu|LinuxMint)$/: {
-						 file { "/etc/modprobe.d/blacklist-dccp.conf":
-									source => "puppet:///modules/ucdpuppet/ubuntu-dccp.conf"
-						 }
-			  }
-			  /^(CentOS|RedHat)$/: {
-						 file { "/etc/modprobe.d/blacklist-dccp.conf":
-									source => "puppet:///modules/ucdpuppet/centos-dccp.conf"
-						 }
-			  }
-
-	}
-
+  augeas { "puppet.conf":
+    context => "/files/etc/puppetlabs/puppet/puppet.conf/agent",
+    changes => ["set server puppet.ucdavis.edu"]
+  }
+  service { 'puppet':
+    enable => true,
+  }
+  #CVE-2017-2636
+  File { "/etc/modprobe.d/disable-n_hdlc.conf":
+    source => ["puppet:///modules/cse/disable-n_hdlc.conf"],
+  }
+  augeas { "floppy.conf":
+    context =>  "/files//etc/modprobe.d/floppy.conf",
+    changes =>  ["set blacklist floppy"]
+  }
+  case $operatingsystem {
+    /^(Debian|Ubuntu|LinuxMint)$/: {
+      file { "/etc/modprobe.d/blacklist-dccp.conf":
+        source => "puppet:///modules/ucdpuppet/ubuntu-dccp.conf"
+      }
+    }
+    /^(CentOS|RedHat)$/: {
+      file { "/etc/modprobe.d/blacklist-dccp.conf":
+        source => "puppet:///modules/ucdpuppet/centos-dccp.conf"
+      }
+    }
+  }
 }
 
